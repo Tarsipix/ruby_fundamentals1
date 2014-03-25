7e9eb004e16cb75930913c274a251ca70b69b6fa
Brian Simon <tarsipix@gmail.com>
2014-03-25 14:06:37 -0400

    second commit

--- exercise2.rb
+++ exercise2.rb
@@ -1,2 +1,13 @@
 # exercise 1
-puts "$55 * 0.10 = $" + (55*0.10).to_s
+puts "To calculate a good tip for a $55 meal, run:"
+puts "> 55 * 0.10"
+puts "Answer: $" + (55*0.10).to_s
+
+# exercise 2
+puts "\nString" + 2343.to_s
+
+# exercise 3
+puts "\n45628 multiplied by 7839 = " + (45628*7839).to_s
+
+# exercise 4
+puts (true && false) || (false && true) || !(false && false)
