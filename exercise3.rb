b5e2c3bb86f9d852c3a5c4d9b535e3dd6cefcf70
Brian Simon <tarsipix@gmail.com>
2014-03-25 14:27:20 -0400

    third commmit

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,6 @@
 puts "What is your name?"
 input = gets.chomp
 puts "Hi #{input}"
+puts "How old are you turning this year?"
+input = gets.chomp.to_i
+puts "You were born in year " + (Time.now.year - input).to_s
