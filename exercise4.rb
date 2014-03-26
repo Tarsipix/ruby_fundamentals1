ac5f2d15c5ac82714a85c02a0b25ef009f8f05a5
Brian Simon <tarsipix@gmail.com>
2014-03-26 15:18:37 -0400

    changed order of if statement to fix issue

--- exercise4.rb
+++ exercise4.rb
@@ -1,11 +1,11 @@
 one_to_onehundred = 1..100
 one_to_onehundred.each do |num|
-  if (num % 3 == 0)
+if (num % 3 == 0 and num % 5 == 0)
+    print "BitMaker"
+  elsif (num % 3 == 0)
     print "Bit"
   elsif (num % 5 == 0)
     print "Maker"
-  elsif (num % 3 == 0 and num % 5 == 0)
-    print "BitMaker"
   else
    print num
  end
